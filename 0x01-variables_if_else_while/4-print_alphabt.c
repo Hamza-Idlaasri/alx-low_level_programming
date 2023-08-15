a679d35e2afdfb14804e89c8e0aecac582385d8a
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-15 07:47:16 -0700

    get letters except 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'q')
+		{
+			lowercase++;
+			continue;
+		}
+		else if (lowercase == 'e')
+		{
+			lowercase++;
+			continue;
+		}
+		else
+		{
+			putchar(lowercase);
+			lowercase++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

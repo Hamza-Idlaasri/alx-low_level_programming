0244f64707310d74fa3720e296f229937ce0257a
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-15 03:54:28 -0700

    display alphabets using putchar only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphab[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	for(i = 0; alphab[i] != '\0' ;i++)
+		putchar(alphab[i]);
+	putchar('\n');
+	return (0);
+}

c8b86dc7e14abccdd6f70e1aded18627c76d5b54
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-16 11:23:25 -0700

    get the last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
- * _isalpha - function who checks is the value alphabet
+ * print_last_digit - function that prints the last digit of a number
  *
- * @c: The character to be checked
+ * @n : is a digit
  *
- * Return: 1 if @c is a letter, 0 otherwise
+ * Return: the last number of a digit
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
+	int r;
+
+	if (n < 0)
+		n = -n;
+	r = n % 10;
+	_putchar(r + '0');
+	return (r);
 }

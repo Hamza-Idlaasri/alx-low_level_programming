765698750da92a01a654b14719965b5f3eabe7a3
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-16 10:43:17 -0700

    get the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - function that computes the abs value of an integer
+ *
+ * @n: The number to be checked
+ *
+ * Return: absolute value of @n
+ */
+int _abs(int n)
+{
+	return abs(n);
+}

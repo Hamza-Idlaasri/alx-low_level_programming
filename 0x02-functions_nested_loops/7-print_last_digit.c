67f3ef35eee8d76d8cec3c5f8d560f4697bb682e
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-16 10:49:17 -0700

    get the absolute value of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - function who checks is the value alphabet
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 if @c is a letter, 0 otherwise
+ */
+int print_last_digit(int)
+{
+}

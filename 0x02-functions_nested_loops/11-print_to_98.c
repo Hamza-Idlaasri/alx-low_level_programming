52f65322457ab3a26bf94c0b39e4e5c90665bc44
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-17 04:20:49 -0700

    reach number 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
+ *
  * print_to_98 - function that prints all natural numbers from n to 98
  *
  * @n: the number was giving
  *
- * Return: 
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while(n < 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
@@ -20,7 +19,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		while(n > 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;

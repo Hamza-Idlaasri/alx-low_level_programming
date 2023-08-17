e99ecf0fb3689777221f19a125ec91756f12fc4f
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-17 04:22:00 -0700

    reach number 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- * print_to_98 - function that prints all natural numbers from n to 98
+ * print_to_98() - function that prints all natural numbers from n to 98
  *
  * @n: the number was giving
- *
  */
 void print_to_98(int n)
 {

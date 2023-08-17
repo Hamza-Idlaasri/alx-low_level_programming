1e7378e26da390f4984dc088e2e3e175ece1abf1
Hamza-Idlaasri <idlaasrihamza7@gmail.com>
2023-08-17 03:09:24 -0700

    give the addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a, @b : the two values giving by the user
+ *
+ * Return: addition of two numbers
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 /* Declare other function prototypes here */
 
 #endif /* MAIN_H */
